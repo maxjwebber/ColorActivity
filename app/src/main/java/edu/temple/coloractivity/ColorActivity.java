9d8f639b1f8bbcc7e39e6c92836937257db614be
maxjwebber <tua83483@temple.edu>
2020-02-13 19:01:27 -0500

    ColorAdapter will now change background color of TextViews to match colors

--- app/src/main/java/edu/temple/coloractivity/ColorActivity.java
+++ app/src/main/java/edu/temple/coloractivity/ColorActivity.java
@@ -2,7 +2,9 @@ package edu.temple.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] colors = getResources().getStringArray(R.array.colors);
+        ColorAdapter adapter = new ColorAdapter(this,colors);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
     }
 }
